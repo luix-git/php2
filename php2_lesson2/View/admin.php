c8b253f5298e4d3d6a09dc6661f4fa0feccbdc46
luix <a.a.vlasov86@gmail.com>
2016-10-20 02:14:51 +0300

    Домашняя работа по уроку php2_lesson2 90% (допилина админка). 	modified:   php2_lesson2/View/admin.php 	modified:   php2_lesson2/admin.php

--- php2_lesson2/View/admin.php
+++ php2_lesson2/View/admin.php
@@ -10,11 +10,24 @@
             <td>title</td>
             <td>article</td>
           </tr>
-        </table>
-        <?php
+          <?php
             foreach ($news as $value) {
-                var_dump($value);
+                echo '<tr>';
+                echo '<td>' . $value->id . '</td>';
+                echo '<td>' . $value->title . '</td>';
+                echo '<td>' . $value->text . '</td>';
+                echo '</tr>';
             }
           ?>
+        </table>
+        <br><br>
+        <form action="admin.php" method="get">
+        id: <input type="number" name="id" value="<?php echo $id;?>"><br><br>
+        title: <input type="text" name="title" size="100" value="<?php echo $title;?>"><br><br>
+        text: <textarea name="text" cols="100" rows="7"><?php echo $text;?></textarea><br><br>
+        <button type="submit" name="method" value="get">Загрузить данные</button>
+        <button type="submit" name="method" value="save">Сохранить</button>
+        <button type="submit" name="method" value="delete">Удалить запись</button>
+    </form>
     </body>
 </html>
--- php2_lesson2/admin.php
+++ php2_lesson2/admin.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . '/App/Other/autoload.php';
-/*
+
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 } else {
@@ -24,18 +24,24 @@ if (isset($_GET['method'])) {
     $method = $_GET['method'];
 } else {
     $method = 'get';
-}*/
+}
 
 $article = new \App\Model\Article();
 $news = $article->findAll();
 
-/*
+
 if ('get' == $method) {
     $data = $article->findById($id);
+    $title = $data->title;
+    $text = $data->text;
 } elseif ('save' == $method) {
+    $article->id = $id;
+    $article->title = $title;
+    $article->text = $text;
     $article->save();
 } elseif ('delete' == $method) {
+    $article->id = $id;
     $article->delete();
-}*/
+}
 
 include __DIR__ . '/View/admin.php';
